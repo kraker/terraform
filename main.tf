ef51d2788a6d1b02482c5d5ac3dc10eeda97d181
Alex Kraker <alex.kraker@benimbl.com>
2023-03-26 13:14:13 -0500

    Update homelab infra

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 resource "proxmox_vm_qemu" "autobots" {
   name        = "autobots"
   target_node = var.target_node
-  clone       = "opensuse15sp4-template"
+  clone       = "openSUSE-Leap-15.4"
   agent       = 1
   os_type     = "cloud-init"
   qemu_os     = "other"
@@ -41,45 +41,6 @@ resource "proxmox_vm_qemu" "autobots" {
   }
 
   ipconfig0 = "ip=192.168.0.201/24,gw=192.168.0.1"
-
-  sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-}
-
-resource "proxmox_vm_qemu" "teleport" {
-  name        = "teleport"
-  target_node = var.target_node
-  clone       = "opensuse15sp4-template"
-  agent       = 1
-  os_type     = "cloud-init"
-  qemu_os     = "other"
-  cores       = 1
-  sockets     = 1
-  cpu         = "host"
-  memory      = 2048
-  scsihw      = "virtio-scsi-pci"
-  bootdisk    = "scsi0"
-
-  disk {
-    slot    = 0
-    size    = "35G"
-    type    = "scsi"
-    storage = "local-zfs"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
-  }
-
-  ipconfig0 = "ip=192.168.0.202/24,gw=192.168.0.1"
 
   sshkeys = <<EOF
     ${var.ssh_key}
--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "autobots_ip" {
   value = proxmox_vm_qemu.autobots.*.default_ipv4_address
 }
 
-output "teleport_ip" {
-  value = proxmox_vm_qemu.teleport.*.default_ipv4_address
-}
-
 output "Win10_ip" {
   value = proxmox_vm_qemu.Win10.*.default_ipv4_address
 }
